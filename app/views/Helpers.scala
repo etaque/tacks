8373f05de3325ed4a2ee14034014cffa267ef350
Emilien Taque <eta@zengularity.com>
2014-10-14 21:40:09 +0200

    fix timer & delta formats

--- app/views/Helpers.scala
+++ app/views/Helpers.scala
@@ -2,12 +2,15 @@ package views
 
 object Helpers {
 
-  def timer(ms: Long) = {
-    val c = Math.ceil(ms / 1000)
-    val m = Math.floor(c / 60)
+  def round3(n: Double): Double = math.round(n * 1000) * 0.001
+
+  def timer(ms: Long): String = {
+    val c = math.ceil(ms / 1000).toInt
+    val m = math.floor(c / 60).toInt
     val s = c % 60
     if (m == 0) s"""$s\""""
     else s"""$m' $s\""""
   }
 
+  def timeDelta(ms: Long): String = "+ " + "%1.3f".format(ms * 0.001) + "\""
 }
